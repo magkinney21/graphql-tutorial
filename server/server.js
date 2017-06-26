0b0836af2030407c7bb94742497b0ffa43454a81
Magkinney21 <magkinney21@gmail.com>
2017-06-26 11:05:14 -0400

    first mutation working on server r

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
-import cors from 'cors';
 import express from 'express';
 import {
   graphqlExpress,
   graphiqlExpress,
 } from 'graphql-server-express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import { schema } from './src/schema';
 
 const PORT = 4000;
 const server = express();
 
-// ... const server = express();
 server.use('*', cors({ origin: 'http://localhost:3000' }));
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({
@@ -23,5 +22,5 @@ server.use('/graphiql', graphiqlExpress({
 }));
 
 server.listen(PORT, () =>
-  console.log(`GraphQL Server is now running on http://localhost:${PORT}/graphiql`)
+  console.log(`GraphQL Server is now running on http://localhost:${PORT}`)
 );
