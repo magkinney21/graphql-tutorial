3943f45e4a00ec31e52bc29070d0d8cad73cbd9d
Magkinney21 <magkinney21@gmail.com>
2017-06-25 21:53:45 -0400

    query running

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
+import { schema } from './src/schema';
+import {
+  graphqlExpress,
+  graphiqlExpress,
+} from 'graphql-server-express';
+import bodyParser from 'body-parser';
 import express from 'express';
 
 const PORT = 4000;
 
 const server = express();
-
-server.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}));
+server.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}));
 
 server.listen(PORT, () => console.log(`GraphQL Server is now running on http://localhost:${PORT}`));
